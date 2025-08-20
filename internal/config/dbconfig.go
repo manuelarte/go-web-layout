b26bb26f14b61ec00b59f54cb8132e682ca6455d
Manuel Doncel Martos <manuel.doncel.martos@ing.com>
2025-08-20 06:54:56 +0200

    adding swagger ui

--- internal/config/dbconfig.go
+++ internal/config/dbconfig.go
@@ -8,6 +8,7 @@ import (
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/sqlite3"
 	"github.com/golang-migrate/migrate/v4/source/iofs"
+
 	"github.com/manuelarte/go-web-layout/resources"
 )
 
